c1baabbfef9ba7360ab6af4f887359f9a1a65f3a
gabymello <mellogaby689@gmail.com>
2025-04-02 14:36:17 -0300

    Add files via upload

---
+++ Aluno/src/aluno/GerenciadorNotas.java
@@ -0,0 +1,17 @@
+
+package aluno;
+
+public class GerenciadorNotas {
+    
+ public String verificarSituacao(double media) {
+        if (media >= 7) {
+            return "Aprovado";
+        } else {
+            return "Reprovado";
+        }
+    }
+}
+
+    
+    
+
